6a83df06bd9fdc389ca6e5351dbf113c4ccb8189
Rodrigo Lavariega Pacheco <17161153@itoaxaca.edu.mx>
2021-12-02 18:06:01 -0600

    se agrega la clase Cuadrupla para el codgio intermedio

---
+++ C-0/src/Analizadores/Cuadrupla.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package C0;
+
+/**
+ *
+ * @author Rodrigo LP
+ */
+class Cuadrupla {
+
+    String nombre;
+    String op1;
+    String op2;
+    String res;
+
+    Cuadrupla(String n, String p1, String p2, String r) {
+        nombre = n;
+        op1 = p1;
+        op2 = p2;
+        res = r;
+    }
+
+    String getNombre() {
+        return nombre;
+    }
+
+    String getOp1() {
+        return op1;
+    }
+
+    String getOp2() {
+        return op2;
+    }
+
+    String getRes() {
+        return res;
+    }
+
+}
