6a5de8185b22da619bb311cf0d138dab5200582f
José Ángel García <chepeaicrag12@gmail.com>
2021-12-06 22:21:00 -0600

    Agregando clase para los mensajes de error

---
+++ C-0/src/Analizadores/Textos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizadores;
+
+/**
+ *
+ * @author lucy_
+ */
+public class Textos {
+    
+    public static final String simboloNoDeclarado = "El simbolo no ha sido declarado previamente";
+    public static final String breakSinWhile = "Se ha utilizado un break fuera de un bloque while";
+    public static final String simboloRedeclarado = "El simbolo ya ha sido declarado previamente";
+    public static final String ficheroCiNoExiste = "El fichero del CI no existe";
+   
+}
