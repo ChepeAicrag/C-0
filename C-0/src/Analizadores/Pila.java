134b603e2125bf413a67fd037dd55133a75c93fe
José Ángel García <chepeaicrag12@gmail.com>
2021-12-07 11:30:43 -0600

    Base de la clase pila para que el sintáctico no marque errores

---
+++ C-0/src/Analizadores/Pila.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizadores;
+
+/**
+ *
+ * @author lucy_
+ */
+class Pila {
+
+    void apilar(int cuentaBucle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int verCima() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void desapilar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
