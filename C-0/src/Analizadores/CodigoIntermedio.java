312a9136cbc070401a9935067ca45c1afe86e8aa
José Ángel García <chepeaicrag12@gmail.com>
2021-12-07 11:29:56 -0600

    Base de la clase código intermedio para que el sintáctico no marque errores

---
+++ C-0/src/Analizadores/CodigoIntermedio.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizadores;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author lucy_
+ */
+public class CodigoIntermedio {
+
+    CodigoIntermedio(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void abrirFicheroEscritura() throws IOException{
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void cerrarFicheroEscritura() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void guardarCuadrupla(Cuadrupla cuadrupla) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
