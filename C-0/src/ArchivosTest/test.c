b7873afc6917e61a759c30005a6cbc0c70df77ca
José Ángel García <chepeaicrag12@gmail.com>
2022-01-07 18:23:48 -0600

    Modificando el archivo de test para verificar que funcione el código final

--- C-0/src/ArchivosTest/test.c
+++ C-0/src/ArchivosTest/test.c
@@ -1,12 +1,18 @@
-int a;
-int b;
-int c;
-main(){
-    putw(a);
-    putw(b);
-    putw(c);
-    a = 4;
-    b = a + c;
-    c = a * b;
-    putw(c);
+int x;
+int y;
+main() {
+    x=1-12;
+    y=x*32;
+    x=(32/12)*y;
+    if(x>7) {
+        while(y>0) {
+            y=y-1;
+            if(y>1) {
+                break;
+            }
+        }
+    }
+puts("Cadena\n");
+puts("de texto");
+putw(x+y);
 }
